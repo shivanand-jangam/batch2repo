7d7b94321c95c20658e477232f3c59c5a8246c29
Shivanand Jangam <shivanand@infoglen.com>
2023-03-15 12:30:52 +0530

    Changes for overloading

--- My-Test-Project/src/main/java/com/test/practice/Polymorphism/OverloadingExample.java
+++ My-Test-Project/src/main/java/com/test/practice/Polymorphism/OverloadingExample.java
@@ -8,13 +8,15 @@ import org.openqa.selenium.support.FindBy;
 public class OverloadingExample {
 
 	WebDriver driver;
-	private static String locator1 = "//a[@title='New']";
 
-	private static By locator2 = By.xpath("//a[@title='New']");
+	private static String locator1 = "//a[@title='bhima']";
 
-	@FindBy(xpath = "//a[@title='New']")
+	private static By locator2 = By.xpath("//a[@title='shiva']");
+
+	@FindBy(xpath = "//a[@title='Srikant']")
 	private static WebElement locator3;
 
+	// With Overloading concept
 	public void clickMethod(String locator) {
 		driver.findElement(By.xpath(locator)).click();
 	}
@@ -23,13 +25,33 @@ public class OverloadingExample {
 		driver.findElement(locator).click();
 	}
 
-	public void clickMethod() {
-		locator3.click();
+	public void clickMethod(WebElement locator) {
+		locator.click();
 	}
 
 	public static void main(String[] args) {
-
+		OverloadingExample example = new OverloadingExample();
+		example.clickMethod(locator3);
+		example.clickMethod(locator1);
+		example.clickMethod(locator2);
 		
+		example.clickUsingWebelemntMethod(locator3);
+		example.clickUsingStringMethod(locator1);
+		example.clickUsingByMethod(locator2);
+		
+	}
+	
+	// Without Overloading concept
+	public void clickUsingStringMethod(String locator) {
+		driver.findElement(By.xpath(locator)).click();
+	}
+
+	public void clickUsingByMethod(By locator) {
+		driver.findElement(locator).click();
+	}
+
+	public void clickUsingWebelemntMethod(WebElement locator) {
+		locator.click();
 	}
 
 }
