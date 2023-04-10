fe7ffeb5619690d39260bd299611dd13d75cb7c3
Bramha <bbramhakar12@gmail.com>
2023-04-10 19:32:08 +0530

    Chnages

--- My-Test-Project/src/test/java/com/test/practice/RegressionSuiteRunner.java
+++ My-Test-Project/src/test/java/com/test/practice/RegressionSuiteRunner.java
@@ -1,4 +1,5 @@
 package com.test.practice;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -13,18 +14,14 @@ import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
 
 @CucumberOptions(features = ".\\src\\test\\java\\com\\test\\resouces\\features", extraGlue = {
-		"\\com\\test\\stepdefination",}, plugin = {"pretty",
-				"html:target/cucumber-reports/cucumber-pretty",
+		"\\com\\test\\stepdefination", }, plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
 				"json:target/cucumber-reports/CucumberTestReport.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-<<<<<<< HEAD
-				"rerun:target/rerun.txt"}, tags = "@smooke")
-=======
-				"rerun:target/rerun.txt"}, tags = "@RegressionTest")
->>>>>>> a461f098b0e52307142def0d8ddcc4596f0bcfb3
+				"rerun:target/rerun.txt" }, tags = "@RegressionTest")
 public class RegressionSuiteRunner<CucumberFeatureWrapper> {
 	private TestNGCucumberRunner testNGCucumberRunner;
 	private BrowserFactory browserFactory;
+
 	@BeforeClass
 	public void beforeClass() {
 		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
@@ -34,10 +31,7 @@ public class RegressionSuiteRunner<CucumberFeatureWrapper> {
 	public void openBrowser() {
 		browserFactory = new BrowserFactory();
 		browserFactory.openBrowser("chrome");
-<<<<<<< HEAD
-=======
 		browserFactory.navigateToUrl();
->>>>>>> a461f098b0e52307142def0d8ddcc4596f0bcfb3
 	}
 
 	@AfterMethod
@@ -46,8 +40,7 @@ public class RegressionSuiteRunner<CucumberFeatureWrapper> {
 	}
 
 	@Test(dataProvider = "scenarios", description = "Scenario Name: ")
-	public void runScenario(PickleWrapper pickleEvent,
-			CucumberFeatureWrapper cucumberFeature) {
+	public void runScenario(PickleWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) {
 		testNGCucumberRunner.runScenario(pickleEvent.getPickle());
 	}
 
